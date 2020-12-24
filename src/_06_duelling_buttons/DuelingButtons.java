4e709c753d530a674fbc7795df3bd4854f115a42
NKonganda <55722447+NKonganda@users.noreply.github.com>
2020-12-23 17:01:53 -0800

    Commit

--- src/_06_duelling_buttons/DuelingButtons.java
+++ src/_06_duelling_buttons/DuelingButtons.java
@@ -18,9 +18,6 @@ public class DuelingButtons implements ActionListener {
 	JButton leftButton = new JButton();
 	JButton rightButton = new JButton();
 
-	Dimension BIG = new Dimension(400, 400);
-	Dimension SMALL = new Dimension(200, 200);
-
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 
